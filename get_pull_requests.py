705150a2cf138ae3fd175168e55cb6b569c5b099
Leonardo Furtado <srleonardofurtado@gmail.com>
2019-01-09 16:33:44 -0300

    Update get_pull_requests.py

--- get_pull_requests.py
+++ get_pull_requests.py
@@ -1,44 +1,20 @@
-"""
-Description.
-
-Script that saves information in a CSV file to
-multiple pull requests from a repository.
-"""
-from github import Github
 import csv
+import math
+from github import Github
 
-# Using acess token
-g = Github("Your token here")
+g = Github("token")
+output_file = open(r'cpython.csv', 'a', encoding='utf8')
+writer = csv.writer(output_file)
+repository = g.get_repo("python/cpython")
 
-# Using username and password
-# g = Github("user", "password")
+result = repository.get_pulls(state="closed")
 
-repository = "mongodb/mongo"
-file_countries = "country.csv"
-
-count = 0
-LIMIT = 1000
-data = open(r'output.csv', 'a', encoding='utf8')
-writer = csv.writer(data)
-repo = g.get_repo(repository)
-
-
-def get_country(city, file):
-    """Through the name of the city, find your country."""
-    with open(file, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in spamreader:
-            if(city == row[0] or city == row[1] or city == row[2] or
-               city == row[3]):
-                return row[2]
-    return "Undefine"
+def get_pull_requests_data():
+    page = math.ceil(result.totalCount//30)
+    for i in range(0, page):
+        print(i)
+        for k in result.get_page(i):
+            writer.writerow([k.user.name, k.user.login, k.user.blog, k.merged, k.user.location])
 
 
-for repo in repo.get_pulls(state="closed"):
-    writer.writerow([repo.user.name, repo.user.login, repo.user.location,
-                    repo.user.blog, repo.merged, get_country(str(repo.user.
-                     location).split(",")[0].lower(), file_countries)])
-    count += 1
-    print(count)
-    if count == LIMIT:
-        break
+get_pull_requests_data()
