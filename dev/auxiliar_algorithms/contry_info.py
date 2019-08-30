3811dcd03c74b00ae0d508a2daf3bb80acd215d6
Leonardo Furtado <srleonardofurtado@gmail.com>
2019-08-30 20:53:06 -0300

    none

--- dev/auxiliar_algorithms/contry_info.py
+++ dev/auxiliar_algorithms/contry_info.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("../../data/bases/fullybase.csv")
 
-df.groupby("country").size().to_csv("dale.csv")
+# df.groupby("country").size().to_csv("dale.csv")
+# print(df.groupby("country").groups["undefined"])
+
+family = np.where((df["merged"] != "False", "accepted", "recused"))
+df.groupby(family)["dale"].mean()
