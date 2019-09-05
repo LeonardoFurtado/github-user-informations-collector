3f1467c96b7a284a21aa6bde8f27b297dd67d418
Leonardo Furtado <srleonardofurtado@gmail.com>
2019-09-05 15:12:51 -0300

    adding country initials

--- dev/get_pull_requests.py
+++ dev/get_pull_requests.py
@@ -13,7 +13,7 @@ from github import Github
 # meu 53b800d95851420a05584f980bbe7d652461963a
 # bot f5a0f7085200659632626e8ef5f0c5c769ba2032
 g = Github("53b800d95851420a05584f980bbe7d652461963a")
-output_file = open(r"../data/bases/spyder.csv", "a", encoding="utf8")
+output_file = open(r"../data/bases/angular.csv", "a", encoding="utf8")
 writer = csv.writer(output_file)
 writer.writerow(
     [
@@ -94,4 +94,4 @@ def get_pull_requests_data(repository):
                 print("requests.exceptions.ReadTimeout")
 
 
-get_pull_requests_data("spyder-ide/spyder")
+get_pull_requests_data("angular/angular")
